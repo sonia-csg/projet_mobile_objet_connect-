d9371c7c74a1f303c8aada1079aa909e8ba60ca5
sonia <sonia@gmail.com>
2024-01-18 15:52:42 +0100

    first init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "projet_arduino_esp32"
+include(":app")
+ 
